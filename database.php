df84b02624d3c8ff36ef0b01e8a099c626874f3c
Matias Volgyi <matiasvolgyi@gmail.com>
2018-08-14 17:39:19 -0300

    fix the db error

---
+++ database.php
@@ -0,0 +1,11 @@
+$active_group = "default";
+$query_builder = TRUE;
+
+$dv['default'] = array(
+    'dns'   => '',
+    'hostname' => 'localhost',
+    'username' => 'ci_user',
+    'password' => 'cipassword',
+    'database' => 'ci_database',
+    'save_quieries' => TRUE
+);
